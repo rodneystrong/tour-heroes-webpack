06bf986907bcd63ca09f96c78540a4d4e7d83e62
aleeex <akang2@gmail.com>
2016-09-05 16:42:02 -0600

    integrating the OnInit interface

--- tour-heroes/src/app/app.component.ts
+++ tour-heroes/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,22 @@ import { HeroService } from './hero.service';
   providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes!';
   selectedHero: Hero;
   constructor(private heroService: HeroService){}
   heroes: Hero[];
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  ngOnInit(): void {
+    
+  }
+
 }
