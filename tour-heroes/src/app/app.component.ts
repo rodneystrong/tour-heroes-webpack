7146287bda31da9cb2c454db1002deca853a167a
aleeex <akang2@gmail.com>
2016-09-05 16:31:17 -0600

    added provider to create instance of HeroService data in app.comp.ts

--- tour-heroes/src/app/app.component.ts
+++ tour-heroes/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
 export class AppComponent {
   title = 'Tour of Heroes!';
   selectedHero: Hero;
+  constructor(private heroService: HeroService){}
   heroes: Hero[];
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
