87495ec1efeffefbf982a4edb66f0259fb681c42
aleeex <akang2@gmail.com>
2016-09-05 13:36:58 -0600

    created new hero-detail component .ts + .html files. moved hero input + details display html to newly created .html

---
+++ tour-heroes/src/app/hero-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component ({
+  selector: 'my-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+//the name below is arbitrary
+export class HeroDetailComponent {
+
+}
